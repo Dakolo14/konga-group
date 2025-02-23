1b3d25f4b3a6547a8f9188d3fce2d2799a0ede80
Ajose Damilare <damilare.ajose@konga.com>
2025-02-23 12:46:57 +0100

    new commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,12 @@
 
 /**  Preload  * Loading will be end after document is loaded  */
 
-const preloader = document.querySelector("[data-preaload]");
+// const preloader = document.querySelector("[data-preaload]");
 
-window.addEventListener("load", function () {
-  preloader.classList.add("loaded");
-  document.body.classList.add("loaded");
-});
+// window.addEventListener("load", function () {
+//   preloader.classList.add("loaded");
+//   document.body.classList.add("loaded");
+// });
 
 /* Add event listeners on multiple events  */
 
